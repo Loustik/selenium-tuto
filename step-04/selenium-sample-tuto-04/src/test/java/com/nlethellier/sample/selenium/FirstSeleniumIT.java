39d41d6f9aa6ee7205021459d2427f5435d0699d
Nicolas L <Loustik@users.noreply.github.com>
2017-10-16 16:48:06 +0200

    uupdate 04

--- step-04/selenium-sample-tuto-04/src/test/java/com/nlethellier/sample/selenium/FirstSeleniumIT.java
+++ step-04/selenium-sample-tuto-04/src/test/java/com/nlethellier/sample/selenium/FirstSeleniumIT.java
@@ -5,7 +5,9 @@ import junit.framework.TestCase;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverService;
 
@@ -31,6 +33,28 @@ public class FirstSeleniumIT extends TestCase{
 	public void testWeSeeLoremIpsum() {
 		driver.get(website.toString());
 		Assert.assertTrue(driver.getPageSource().contains("Lorem Ipsum"));
+	}
+	
+	@Test
+	public void testPageTitle() {
+		driver.get(website.toString());
+		Assert.assertEquals("Selenium sample tuto", driver.getTitle());
+	}
+	
+	@Test
+	public void testGetElementById() {
+		driver.get(website.toString());
+		WebElement newParagraph = driver.findElement(By.id("new_paragraph"));
+		Assert.assertEquals("This is a brand new paragraph", newParagraph.getText());
+	}
+	
+	@Test
+	public void interactWithPage() {
+		driver.get(website.toString());
+		WebElement loremParagraph = driver.findElement(By.id("lorem"));
+		loremParagraph.clear();
+		loremParagraph.sendKeys("New Lorem Ipsum paragraph, edited by Selenium");
+		Assert.assertEquals("New Lorem Ipsum paragraph, edited by Selenium", loremParagraph.getText());
 	}
 	
 }
