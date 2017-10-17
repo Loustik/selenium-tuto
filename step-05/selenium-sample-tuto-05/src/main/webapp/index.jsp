6a5975bfd9f0157eed2579d7499418d7b31e484c
Nicolas L <Loustik@users.noreply.github.com>
2017-10-17 10:37:27 +0200

    update 05.1

---
+++ step-05/selenium-sample-tuto-05/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+	<title>Selenium sample tuto</title>
+</head>
+<body>
+<h2>Hey, this is the default page !</h2>
+	<div>
+	    <header>
+	        <nav class="menu">
+	            <a href="index.jsp" title="Home">Home</a> |
+	            <a href="send-data.jsp">Send data</a>
+	        </nav>
+	    </header>
+	    <p id='lorem'>Lorem Ipsum bla bla bla...</p>
+      <p id='new_paragraph'>This is a brand new paragraph</p>
+	    <footer><small>&copy;Selenium makes life easier :)</small></footer>
+	</div>
+</body>
+</html>
---
+++ step-05/selenium-sample-tuto-05/src/test/java/com/nlethellier/sample/selenium/FirstSeleniumIT.java
@@ -0,0 +1,49 @@
+package com.nlethellier.sample.selenium;
+
+import java.net.URI;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumIT extends AbstractIT {
+	private URI website;
+	private WebDriver driver;
+
+	@Override
+	protected void setUp() throws Exception { 
+		super.setUp();
+		website = new URI("http://localhost:8081/selenium-sample-tuto-04");
+		driver = new FirefoxDriver();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		driver.close();
+	}
+	
+	
+	@Test
+	public void testWeSeeLoremIpsum() {
+		driver.get(website.toString());
+		Assert.assertTrue(driver.getPageSource().contains("Lorem Ipsum"));
+	}
+	
+	@Test
+	public void testPageTitle() {
+		driver.get(website.toString());
+		Assert.assertEquals("Selenium sample tuto", driver.getTitle());
+	}
+	
+	@Test
+	public void testGetElementById() {
+		driver.get(website.toString());
+		WebElement newParagraph = driver.findElement(By.id("new_paragraph"));
+		Assert.assertEquals("This is a brand new paragraph", newParagraph.getText());
+	}
+	
+}
