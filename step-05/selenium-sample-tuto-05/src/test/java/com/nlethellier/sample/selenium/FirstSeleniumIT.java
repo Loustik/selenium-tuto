aa2976ae614f732f6e95f60eab82dcf5b40d0ce9
Nicolas L <Loustik@users.noreply.github.com>
2017-10-18 03:50:07 +0200

    debug 5

--- step-05/selenium-sample-tuto-05/src/test/java/com/nlethellier/sample/selenium/FirstSeleniumIT.java
+++ step-05/selenium-sample-tuto-05/src/test/java/com/nlethellier/sample/selenium/FirstSeleniumIT.java
@@ -27,32 +27,29 @@ public class FirstSeleniumIT extends TestCase{
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
-		website = new URI("http://localhost:8081/selenium-sample-tuto-05");
-		driver = new FirefoxDriver();
+		driver.get(website.toString());
 	}
 
-	@Override
-	public void tearDown() throws Exception {
-		super.tearDown();
-		driver.close();
-	}
+//	@Override
+//	public void tearDown() throws Exception {
+//		super.tearDown();
+//		driver.close();
+//	}
 	
 	
 	@Test
 	public void testWeSeeLoremIpsum() {
-		driver.get(website.toString());
+		
 		Assert.assertTrue(driver.getPageSource().contains("Lorem Ipsum"));
 	}
 	
 	@Test
 	public void testPageTitle() {
-		driver.get(website.toString());
 		Assert.assertEquals("Selenium sample tuto", driver.getTitle());
 	}
 	
 	@Test
 	public void testGetElementById() {
-		driver.get(website.toString());
 		WebElement newParagraph = driver.findElement(By.id("new_paragraph"));
 		Assert.assertEquals("This is a brand new paragraph", newParagraph.getText());
 	}
