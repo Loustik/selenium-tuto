d8ff58c5d0afe0c367cb190cae4ead0f3a14c36d
Nicolas L <Loustik@users.noreply.github.com>
2017-10-18 22:35:45 +0200

    update 10

--- step-10/selenium-sample-tuto-10/src/test/java/com/nlethellier/sample/selenium/CreateUserPageIT.java
+++ step-10/selenium-sample-tuto-10/src/test/java/com/nlethellier/sample/selenium/CreateUserPageIT.java
@@ -35,8 +35,33 @@ public class CreateUserPageIT extends AbstractIT {
 	}
 
 	@Test
-	public void testSubmit() {
-		createUserPage.submit("jean-pierre", "FOUCAULT", "12 ans");
+	public void testSubmit1() {
+		createUserPage.submit("Chuck", "NORRIS", "499 ans");
+	}
+	
+	@Test
+	public void testSubmit2() {
+		createUserPage.submit("Jean-Pierre", "FOUCAULT", "12 ans");
+	}
+	
+	@Test
+	public void testSubmit3() {
+		createUserPage.submit("Bob", "L'éponge", "et son ami Patrick");
+	}
+	
+	@Test
+	public void testSubmit4() {
+		createUserPage.submit("Merci Jean-François", "pour la formation", "dans la PAF");
+	}
+	
+	@Test
+	public void testSubmit5() {
+		createUserPage.submit("Selenium", "C'est", "Trop d'la balle");
+	}
+	
+	@Test
+	public void testSubmit6() {
+		createUserPage.submit("Je place des C@R@ct€r€5 spéciaux", "dans le texte", "ce n'est pas un nombre");
 	}
 }
 
